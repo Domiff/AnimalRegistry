a467c7cf7a76499e2aa748f3883daf822972b14a
Domitori <dmlegasy@gmail.com>
2025-04-18 13:17:37 +0300

    Corrected main.py

--- database/main.py
+++ database/main.py
@@ -7,11 +7,11 @@ from insert_data_in_table_pack_animals import insert_data_into_table_pack_animal
 
 
 def main():
-    # create_tables()
-    # insert_data_into_table_pets()
-    # insert_data_into_table_pack_animal()
-    # delete_data()
-    # create_table_young_animals()
+    create_tables()
+    insert_data_into_table_pets()
+    insert_data_into_table_pack_animal()
+    delete_data()
+    create_table_young_animals()
     create_table_for_all_animals()
 
 
